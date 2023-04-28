0fd62cdca40fc1dfa152c7a4b12c3877187bf81c
DanyloSih <geratosfen@gmail.com>
2023-04-28 15:50:01 +0300

    Created Miss Manager

---
+++ Assets/Scripts/UI/MissView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AlienProduction.UI
+{
+    public class MissView : MonoBehaviour
+    {
+        [SerializeField] private Sprite _availableMissTexture;
+        [SerializeField] private Sprite _unavailableMissTexture;
+
+        [SerializeField] private Image _missImage;
+
+        public void SetAvailableStyle()
+        {
+            _missImage.sprite = _availableMissTexture;
+        }
+
+        public void SetUnavailableStyle()
+        {
+            _missImage.sprite = _unavailableMissTexture;
+        }
+    }
+}
